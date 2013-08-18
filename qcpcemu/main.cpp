4bfa05f0bfbc7c01274223cbf461cb22f45d3393
Christian Loose <christian.loose@hamburg.de>
2013-08-18 14:57:54 +0200

    revert changes to main.cpp

--- qcpcemu/main.cpp
+++ qcpcemu/main.cpp
@@ -1,37 +1,8 @@
 #include <QApplication>
 #include "mainwindow.h"
 
-#include <QDebug>
-#include <QFile>
-#include <QTextStream>
-
-void myMessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
-{
-    QString txt;
-    switch (type) {
-    case QtDebugMsg:
-        txt = msg;
-        break;
-    case QtWarningMsg:
-        txt = QString("Warning: %1").arg(msg);
-        break;
-    case QtCriticalMsg:
-        txt = QString("Critical: %1").arg(msg);
-        break;
-    case QtFatalMsg:
-        txt = QString("Fatal: %1").arg(msg);
-        abort();
-    }
-
-    QFile outFile("log.txt");
-    outFile.open(QIODevice::WriteOnly | QIODevice::Append);
-    QTextStream ts(&outFile);
-    ts << txt << endl;
-}
-
 int main(int argc, char *argv[])
 {
-    qInstallMessageHandler(myMessageHandler);
     QApplication a(argc, argv);
 
     MainWindow w;
